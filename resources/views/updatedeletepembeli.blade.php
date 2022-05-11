7fcbf82d532be5c78048851f8f818867b25bae57
AnandaSuryaWedhana <75228944+AnandaSuryaWedhana@users.noreply.github.com>
2022-05-11 17:32:57 +0700

    Update updatedeletepembeli.blade.php

--- resources/views/updatedeletepembeli.blade.php
+++ resources/views/updatedeletepembeli.blade.php
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Hello, world!</title>
+    <title>{{ $title }}</title>
   </head>
   <body>
       @include('partials.partialnavbar')
@@ -36,7 +36,7 @@
               <td>Lorem ipsum dolor</td>
               <td >Lor</td>
               <td>
-                <a href="#"><button type="button" class="btn btn-info">Edit</button></a>
+                <a href="/updatepembeli"><button type="button" class="btn btn-info">Edit</button></a>
                 <a href="#"><button type="button" class="btn btn-danger">Delete</button></a>
               </td>
             </tr>
