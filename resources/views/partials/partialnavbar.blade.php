33052ef40239f6cf00ddfeb3d0fa8673267316e9
AnandaSuryaWedhana <75228944+AnandaSuryaWedhana@users.noreply.github.com>
2022-05-11 18:08:36 +0700

    Update partialnavbar.blade.php

--- resources/views/partials/partialnavbar.blade.php
+++ resources/views/partials/partialnavbar.blade.php
@@ -61,22 +61,11 @@
           <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" style="margin-right : 10px" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Nama
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <li><a class="dropdown-item" href="#">Action</a></li>
-                      <li><a class="dropdown-item" href="#">Another action</a></li>
-                      <li><hr class="dropdown-divider"></li>
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                  </li>
-              </li>
-              <li class="nav-item">
-                <button class="btn btn-outline-warning me-2" type="button">Log Out
-                </button>
+                <a class="nav-link disabled" style="color: whitesmoke">Nama</a>
             </li>
+                <li class="nav-item">
+                    <button class="btn btn-outline-warning me-2" type="button">Log Out</button>
+                </li>
             </ul>
           </div>
         </div>
