de602a232f7bcce9150477cb04efb769989163e3
AnandaSuryaWedhana <75228944+AnandaSuryaWedhana@users.noreply.github.com>
2022-05-12 17:22:05 +0700

    Update partialnavbarmarketing.blade.php

--- resources/views/partials/partialnavbarmarketing.blade.php
+++ resources/views/partials/partialnavbarmarketing.blade.php
@@ -19,7 +19,7 @@
             <ul class="navbar-nav">
               <li class="nav-item">
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle {{ ($title === "portofolioprodukmarketing") ? "active" : "" }}" style="margin-right : 10px" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle {{ ($title === "portofolioprodukmarketing" or $title === "detailportofolioprodukmarketing") ? "active" : "" }}" style="margin-right : 10px" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                           Produk
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
