29cd600a17b05ce75f00e067f67c51e361bfeda2
AnandaSuryaWedhana <75228944+AnandaSuryaWedhana@users.noreply.github.com>
2022-05-13 09:33:41 +0700

    Update updatedeletekategori.blade.php

--- resources/views/updatedeletekategori.blade.php
+++ resources/views/updatedeletekategori.blade.php
@@ -94,7 +94,7 @@
                     <a href="/deletekategori"><button type="button" class="btn btn-danger">Delete</button></a>
                   </td>
               </tr>
-            
+
           </tbody>
 
 
@@ -103,6 +103,5 @@
 
       </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
