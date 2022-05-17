cf8fb27bcd1a325be5b8a696ff5ee90446a5ba45
AnandaSuryaWedhana <75228944+AnandaSuryaWedhana@users.noreply.github.com>
2022-05-18 00:22:01 +0700

    Update updatedeletepembeli.blade.php

--- resources/views/updatedeletepembeli.blade.php
+++ resources/views/updatedeletepembeli.blade.php
@@ -4,8 +4,8 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- Bootstrap CSS -->
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>{{ $title }}</title>
   </head>
@@ -21,9 +21,11 @@
           <!--Table head-->
           <thead>
             <tr class="text-center">
-              <th>Number</th>
-              <th class="th-lg">Username</th>
-              <th class="th-lg">Password</th>
+              <th>ID PEMBELI</th>
+              <th class="th-lg">NAMA</th>
+              <th class="th-lg">ALAMAT</th>
+              <th class="th-lg">NOMOR TELEPON</th>
+              <th class="th-lg">JUMLAH PEMBELIAN PRODUK</th>
               <th class="th-lg">Action</th>
             </tr>
           </thead>
@@ -31,33 +33,19 @@
 
           <!--Table body-->
           <tbody>
-            <tr class="align-middle text-center">
-              <th scope="row">1</th>
-              <td>Lorem ipsum dolor</td>
-              <td >Lor</td>
-              <td>
-                <a href="/updatepembeli"><button type="button" class="btn btn-info">Edit</button></a>
-                <a href="#"><button type="button" class="btn btn-danger">Delete</button></a>
-              </td>
-            </tr>
-            <tr class="align-middle text-center">
-              <th scope="row">1</th>
-              <td>Lorem ipsum dolor</td>
-              <td >Lor</td>
-              <td>
-                <button type="button" class="btn btn-info">Edit</button>
-                <button type="button" class="btn btn-danger">Delete</button>
-              </td>
-            </tr>
-            <tr class="align-middle text-center">
-              <th scope="row">1</th>
-              <td>Lorem ipsum dolor</td>
-              <td >Lor</td>
-              <td>
-                <button type="button" class="btn btn-info">Edit</button>
-                <button type="button" class="btn btn-danger">Delete</button>
-              </td>
-            </tr>
+              @foreach ($list as $data)
+              <tr class="align-middle text-center">
+                <th scope="row">{{ $data->ID_PEMBELI }}</th>
+                <td>{{ $data->NAMA_PEMBELI }}</td>
+                <td >{{ $data->ALAMAT_PEMBELI }}</td>
+                <td >{{ $data->TELPHONE_PEMBELI }}</td>
+                <td >{{ $data->JUMLAHPRODUK_TRANSAKSI }}</td>
+                <td>
+                  <a href="/updatepembeli/{{ $data->ID_PEMBELI }}"><button type="button" class="btn btn-info">Edit</button></a>
+                  <a href="#"><button type="button" class="btn btn-danger">Delete</button></a>
+                </td>
+              </tr>
+              @endforeach
           </tbody>
 
 
