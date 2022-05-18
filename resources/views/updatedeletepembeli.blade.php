da4950fbe1e764dfe79740b6f486b003a982c294
AnandaSuryaWedhana <75228944+AnandaSuryaWedhana@users.noreply.github.com>
2022-05-18 22:12:26 +0700

    Update updatedeletepembeli.blade.php

--- resources/views/updatedeletepembeli.blade.php
+++ resources/views/updatedeletepembeli.blade.php
@@ -18,6 +18,12 @@
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
         @endif
+        @if (Session::get('successupdate'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ Session::get('successupdate') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
         <h1 class="text-center">Update & Delete Pembeli</h1>
       <div class="table-responsive mt-4">
 
