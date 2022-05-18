5e533bd15a3b2d73af71dd050c0cf8c7f6e2dcb1
AnandaSuryaWedhana <75228944+AnandaSuryaWedhana@users.noreply.github.com>
2022-05-18 22:12:44 +0700

    Update updatepembeli.blade.php

--- resources/views/updatepembeli.blade.php
+++ resources/views/updatepembeli.blade.php
@@ -13,22 +13,29 @@
     @include('partials.partialnavbar')
     <div class="container bg-light mt-4" style="padding:20px; border-radius:10px; border:0.1px solid rgb(201, 201, 201)">
         <h1 class="text-center">Ubah Pembeli</h1>
-    <form action="" method="post">
-      <label for="exampleFormControlInput1" class="form-label">Nama Pembeli :</label>
-      <input class="form-control" value="{{ $data[0]->NAMA_PEMBELI }}" type="text" placeholder="Nama Customer" aria-label="default input example" required/>
+    <form action="/updatepembeli/{{ $data[0]->ID_PEMBELI }}" method="post">
+        @csrf
+        <div class="col-12" style="padding-top: 10px">
+            <label for="inputName" class="form-label">Nama : </label>
+            <input type="text" name="Nama" class="form-control" id="inputName" value="{{ $data[0]->NAMA_PEMBELI }}"/>
+            <span style="color: red">@error('Nama'){{ $message }}@enderror</span>
+        </div>
       <div class="col-12" style="padding-top: 10px">
         <label for="inputAddress" class="form-label">Alamat : </label>
-        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" value="{{ $data[0]->ALAMAT_PEMBELI }}" required/>
+        <input type="text" name="Alamat" class="form-control" id="inputAddress" placeholder="1234 Main St" value="{{ $data[0]->ALAMAT_PEMBELI }}" />
+        <span style="color: red">@error('Alamat'){{ $message }}@enderror</span>
       </div>
       <div class="seperatediv" style="margin-top: 10px; display:flex">
         <div class="col-md-6" style="width: 70%">
           <label for="inputAddress" class="form-label">Nomor Telpon :</label>
-          <input type="tel" class="form-control" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" value="{{ $data[0]->TELPHONE_PEMBELI }}" required />
+          <input type="tel" name="NomorTelepon" class="form-control" value="{{ $data[0]->TELPHONE_PEMBELI }}"  />
+          <span style="color: red">@error('NomorTelepon'){{ $message }}@enderror</span>
         </div>
         <div class="space" style="width: 5%"></div>
         <div class="mb-3" style="width: 25%">
           <label for="exampleFormControlInput1" class="form-label">Jumlah Transaksi :</label>
-          <input type="number" class="form-control" id="exampleFormControlInput1" value="{{ $data[0]->JUMLAHPRODUK_TRANSAKSI }}" required/>
+          <input type="number" name="JumlahTransaksi" class="form-control" id="exampleFormControlInput1" value="{{ $data[0]->JUMLAHPRODUK_TRANSAKSI }}" />
+          <span style="color: red">@error('JumlahTransaksi'){{ $message }}@enderror</span>
         </div>
       </div>
       <div class="col-12 text-center">
