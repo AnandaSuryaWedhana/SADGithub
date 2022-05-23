5d35eee2f404cd4384905e127adbdff2ec8c11fa
AnandaSuryaWedhana <75228944+AnandaSuryaWedhana@users.noreply.github.com>
2022-05-23 15:16:37 +0700

    Update laporanproduklaris.blade.php

--- resources/views/laporanproduklaris.blade.php
+++ resources/views/laporanproduklaris.blade.php
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Hello, world!</title>
+    <title>{{ $title }}</title>
   </head>
   <body>
     @include('partials.partialnavbar')
@@ -32,49 +32,17 @@
 
           <!--Table body-->
           <tbody>
-            <tr class="align-middle text-center">
-              <th scope="row">1</th>
-              <td>Lorem ipsum dolor</td>
-              <td >xxx</td>
-              <td >xxx</td>
-              <td >xxx</td>
-              <td >xxx</td>
-            </tr>
-            <tr class="align-middle text-center">
-                <th scope="row">2</th>
-                <td>Lorem ipsum dolor</td>
-                <td >xxx</td>
-                <td >xxx</td>
-                <td >xxx</td>
-                <td >xxx</td>
-              </tr>
+              @foreach ($listdata as $data)
               <tr class="align-middle text-center">
-                <th scope="row">3</th>
-                <td>Lorem ipsum dolor</td>
-                <td >xxx</td>
-                <td >xxx</td>
-                <td >xxx</td>
-                <td >xxx</td>
+                <th scope="row">{{ $data->ID_PRODUK }}</th>
+                <td>{{ $data->NAMA_KATEGORI }}</td>
+                <td >{{ $data->NAMA_PRODUK }}</td>
+                <td >{{ $data->DESKRIPSI_PRODUK }}</td>
+                <td >{{ $data->HARGA_PRODUK }}</td>
+                <td >{{ $data->JUMLAHPRODUK_TRANSAKSI }}</td>
               </tr>
-              <tr class="align-middle text-center">
-                <th scope="row">4</th>
-                <td>Lorem ipsum dolor</td>
-                <td >xxx</td>
-                <td >xxx</td>
-                <td >xxx</td>
-                <td >xxx</td>
-              </tr>
-              <tr class="align-middle text-center">
-                <th scope="row">5</th>
-                <td>Lorem ipsum dolor</td>
-                <td >xxx</td>
-                <td >xxx</td>
-                <td >xxx</td>
-                <td >xxx</td>
-              </tr>
+              @endforeach
           </tbody>
-
-
         </table>
       </div>
     </div>
