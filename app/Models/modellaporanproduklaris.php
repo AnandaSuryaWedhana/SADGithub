c0f712b8285268ce62b1c9e83b0fa93f74408544
AnandaSuryaWedhana <75228944+AnandaSuryaWedhana@users.noreply.github.com>
2022-05-25 15:49:32 +0700

    Create modellaporanproduklaris.php

---
+++ app/Models/modellaporanproduklaris.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class modellaporanproduklaris extends Model
+{
+    use HasFactory;
+    protected $table = 'PRODUK';
+    protected $primarykey = 'ID_PRODUK';
+}
