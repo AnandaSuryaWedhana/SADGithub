39b4faf698ff42b353397be6faaaed178301082d
AnandaSuryaWedhana <75228944+AnandaSuryaWedhana@users.noreply.github.com>
2022-05-25 18:46:28 +0700

    Update detailportofolioprodukmarketing.blade.php

--- resources/views/detailportofolioprodukmarketing.blade.php
+++ resources/views/detailportofolioprodukmarketing.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ $title }}</title>
     <style>
         .container1 {
             width: 45%;
@@ -19,18 +19,18 @@
     <div class="container mt-4 mb-4">
         <h1 class="text-center">Detail Produk</h1>
     </div>
-    <div class="container mt-4" style="display: flex">
+    <div class="container mt-4 mb-4" style="display: flex">
             <div class="container1">
                 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                       <div class="carousel-item active">
-                        <img src="photo/nakasphoto.jpg" class="d-block rounded" style="width : 100%" alt="...">
+                        <img src="{{ $photo }}" class="d-block rounded" style="width : 100%" alt="...">
                       </div>
                       <div class="carousel-item">
-                        <img src="photo/nakasphoto.jpg" class="d-block rounded" style="width : 100%" alt="...">
+                        <img src="{{ $photo }}" class="d-block rounded" style="width : 100%" alt="...">
                       </div>
                       <div class="carousel-item">
-                        <img src="photo/nakasphoto.jpg" class="d-block rounded" style="width : 100%" alt="...">
+                        <img src="{{ $photo }}" class="d-block rounded" style="width : 100%" alt="...">
                       </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
@@ -47,19 +47,19 @@
             <div class="container2" style="width: 50%">
                 <div class="col-12">
                     <label for="inputName" class="form-label">Nama Produk : </label>
-                    <input type="text" class="form-control" value="Nakas 70 X 50 X 50" aria-label="readonly input example" readonly/>
+                    <input type="text" class="form-control" value="{{ $data[0]->NAMA_PRODUK }}" aria-label="readonly input example" readonly/>
                 </div>
                 <div class="mb-3 mt-2">
                     <label for="exampleFormControlTextarea1" class="form-label">Deskripsi Produk : </label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" value="8" aria-label="readonly input example" readonly></textarea>
+                    <input type="text" class="form-control" value="{{ $data[0]->DESKRIPSI_PRODUK }}" aria-label="readonly input example" readonly/>
                 </div>
                 <div class="col-12">
                     <label for="inputName" class="form-label">Harga Produk : </label>
-                    <input type="text" class="form-control" value="8" aria-label="readonly input example" readonly/>
+                    <input type="text" class="form-control" value="{{ $data[0]->HARGA_PRODUK }}" aria-label="readonly input example" readonly/>
                 </div>
                 <div class="col-12 mt-3">
                     <label for="inputName" class="form-label">Jumlah Transaksi Produk : </label>
-                    <input type="text"  class="form-control" value="8" aria-label="readonly input example" readonly/>
+                    <input type="text"  class="form-control" value="{{ $data[0]->JUMLAHPRODUK_TRANSAKSI }}" aria-label="readonly input example" readonly/>
                 </div>
             </div>
     </div>
