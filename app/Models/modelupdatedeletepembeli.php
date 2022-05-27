c1fccd271a6c5348f8be6f7ab7071cc2cffd3c80
AnandaSuryaWedhana <75228944+AnandaSuryaWedhana@users.noreply.github.com>
2022-05-28 00:37:08 +0700

    Update modelupdatedeletepembeli.php

--- app/Models/modelupdatedeletepembeli.php
+++ app/Models/modelupdatedeletepembeli.php
@@ -4,11 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Kyslik\ColumnSortable\Sortable;
 
 class modelupdatedeletepembeli extends Model
 {
     use HasFactory;
+    use Sortable;
     protected $table = 'PEMBELI';
     protected $primarykey = 'ID_PEMBELI';
     public $timestamp = false;
+    public $sortable = [
+        'ID_PEMBELI','NAMA_PEMBELI','ALAMAT_PEMBELI','TELPHONE_PEMBELI','JUMLAHPRODUK_TRANSAKSI'
+    ];
 }
