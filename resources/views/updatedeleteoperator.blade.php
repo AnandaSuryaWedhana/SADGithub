a9486d8b9db91f03148073b245894c87947b8fba
AnandaSuryaWedhana <75228944+AnandaSuryaWedhana@users.noreply.github.com>
2022-06-03 20:47:17 +0700

    Update updatedeleteoperator.blade.php

--- resources/views/updatedeleteoperator.blade.php
+++ resources/views/updatedeleteoperator.blade.php
@@ -7,6 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
     <title>{{ $title }}</title>
   </head>
   <body>
@@ -25,18 +26,18 @@
             </div>
         @endif
         <h1 class="text-center">Update & Delete Operator</h1>
-        <form method="get">
+        {{-- <form method="get">
             <div class="form-group row mb-4 mt-4">
               <label for="exampleDataList" class="col-sm-2 col-form-label">Cari Data :</label>
               <div class="col-sm-10">
                 <input class="form-control" type="text" name="searchside" autofocus="true" list="datalistOptions" id="exampleDataList" placeholder="Type to search..." value="{{ $inputdata }}">
               </div>
             </div>
-          </form>
+          </form> --}}
       <div class="table-responsive mt-4">
 
         <!--Table-->
-        <table class="table table-striped table-hover ">
+        <table class="table table-striped table-hover" id="operatortable">
 
           <!--Table head-->
           <thead>
@@ -65,7 +66,14 @@
           </tbody>
         </table>
       </div>
-      <div class="d-flex justify-content-center">{!! $list->appends(Request::except('page'))->render() !!}</div>
+      {{-- <div class="d-flex justify-content-center">{!! $list->appends(Request::except('page'))->render() !!}</div> --}}
     </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+   <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+   <script>
+       $(document).ready( function () {
+        $('#operatortable').DataTable();
+        } );
+   </script>
   </body>
 </html>
