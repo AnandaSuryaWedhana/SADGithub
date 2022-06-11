da9eeacd9d2537866d16a75a0de623d2a81ecda2
AnandaSuryaWedhana <75228944+AnandaSuryaWedhana@users.noreply.github.com>
2022-06-11 22:45:45 +0700

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,35 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel = "icon" href = "a.jpg" type = "image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
     <link rel="stylesheet" href="css/login.css"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide">
     <title>Login</title>
 </head>
 <body>
+
     <div class="center">
         <h1>Login</h1>
-        <form method="post">
+        <form action="/loginuser" method="POST">
+            @csrf
             <div class="txt_field">
-                <input type="text" required>
-                <span></span>
+                <input type="text" name="username" required>
                 <label>Username</label>
             </div>
             <div class="txt_field">
-                <input type="password" required>
-                <span></span>
+                <input type="password" name="password" required>
                 <label>Password</label>
             </div>
-            <div class="pass" href="{{ url('/forgotpassword') }}"><a href="{{ url('/forgotpassword') }}">Forgot Password?</a></div>
             <input type="submit" value="Login" id="submitBtn">
-            <script>
-            document.getElementById("submitBtn").addEventListener("click", login);
-            function login() {
-                window.location.href="href="{{ url('/home') }}"";
-            }
-            </script>
         </form>
+        @if (Session::get('fail'))
+            <div class="container alert alert-danger alert-dismissible fade show mb-4 mt-3" role="alert">
+                {{ Session::get('fail') }}
+            </div>
+        @endif
     </div>
 </body>
 </html>
