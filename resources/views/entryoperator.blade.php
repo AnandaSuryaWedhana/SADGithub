d051dba8ce30ad615088fde2e187e36e9b16e3da
AnandaSuryaWedhana <75228944+AnandaSuryaWedhana@users.noreply.github.com>
2022-06-11 22:54:47 +0700

    Update entryoperator.blade.php

--- resources/views/entryoperator.blade.php
+++ resources/views/entryoperator.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="signup.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
   </head>
-  <body>
+  <body style="background: linear-gradient(120deg,rgb(248, 233, 166),#fcb462);">
     @include('partials.partialnavbar')
     <div class="container bg-light" style="padding:20px; border-radius:10px; border:0.1px solid rgb(201, 201, 201); margin-top:10px">
         <h1 class="text-center">Menambah Operator</h1>
