e515c27ff8db46ebca49d294798971e7847158f1
AnandaSuryaWedhana <75228944+AnandaSuryaWedhana@users.noreply.github.com>
2022-06-16 21:40:27 +0700

    Update updatedeleteoperator.blade.php

--- resources/views/updatedeleteoperator.blade.php
+++ resources/views/updatedeleteoperator.blade.php
@@ -26,6 +26,12 @@
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         @endif
+        @if (Session::get('fail'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ Session::get('fail') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
         {{-- <form method="get">
             <div class="form-group row mb-4 mt-4">
               <label for="exampleDataList" class="col-sm-2 col-form-label">Cari Data :</label>
